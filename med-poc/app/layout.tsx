6b1cd1508784ea477401ced53c4b6fec519beed7
nrg <itsenergyy05@gmail.com>
2025-05-21 22:17:12 -0400

    add sidebar

--- med-poc/app/layout.tsx
+++ med-poc/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+    <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <main>
+        <SidebarTrigger />
         {children}
+      </main>
+    </main>
+    </SidebarProvider>
       </body>
     </html>
   );
