a06a2dcaa1583f9251479a30ce6cdba7a8e94b14
cAugustoSantana <c300698@gmail.com>
2025-05-28 00:00:36 -0400

    Added Patient Table

--- med-poc/app/patient/page.tsx
+++ med-poc/app/patient/page.tsx
@@ -1,17 +1,16 @@
 // app/page.tsx
 import React from 'react';
 import {patient} from '../patient/data.json';
+import TableBlock  from '@/components/comp-485';
 
 export default function HomePage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">
-          Welcome to My Next.js App
+         Patients
         </h1>
-        <p className="text-lg text-gray-600">
-          {patient.name}
-        </p>
+        <TableBlock />
       </div>
     </main>
   );
