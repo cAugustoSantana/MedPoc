84b97f10c52a144d5e39cd4aeb44b77f3f731580
iEnergyy <44242768+iEnergyy@users.noreply.github.com>
2025-06-02 18:54:56 -0700

    Add db (#1)
    * add supabase-drizzle
    
    * extreme makeover
    
    * init schema

---
+++ db/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv'
+import { join } from 'path'
+
+// Go up to the root
+config({ path: join(__dirname, '../.env') })
+export default defineConfig({
+  out: './migrations',
+  schema: './schema',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
