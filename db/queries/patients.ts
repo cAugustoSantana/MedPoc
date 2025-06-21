2eb0726afce0904c0ffc296882759e9b6ef3e8b6
nrg <itsenergyy05@gmail.com>
2025-06-21 12:54:23 -0400

    update - query

--- db/queries/patients.ts
+++ db/queries/patients.ts
@@ -1,6 +1,7 @@
 import { Patient } from "@/types/patient";
 import { db } from "../index";
 import { patient } from "../migrations/schema";
+import { eq } from "drizzle-orm";
 
 export async function getAllPatients(): Promise<Patient[]> {
   const patients = await db.select().from(patient);
@@ -40,6 +41,31 @@ export async function createPatient(newPatient: Patient): Promise<Patient> {
     address: createdPatient[0].address ?? '',
     createdAt: createdPatient[0].createdAt ?? '',
     updatedAt: createdPatient[0].updatedAt ?? '',
+  }
+  return patientData;
+}
+
+export async function updatePatient(id: number, updatedPatient: Patient): Promise<Patient> {
+  const parsedPatient = {
+    name: updatedPatient.name,
+    email: updatedPatient.email,
+    dob: updatedPatient.dob,
+    gender: updatedPatient.gender,
+    phone: updatedPatient.phone,
+    address: updatedPatient.address,
+  }
+  const updatePatient = await db.update(patient).set(parsedPatient).where(eq(patient.patientId, id)).returning();
+  console.log("Updated patient:", updatePatient);
+  const patientData: Patient = {
+    id: updatePatient[0].patientId.toString(),
+    name: updatePatient[0].name,
+    email: updatePatient[0].email ?? '',
+    dob: updatePatient[0].dob ?? '',
+    gender: updatePatient[0].gender ?? '',
+    phone: updatePatient[0].phone ?? '',
+    address: updatePatient[0].address ?? '',
+    createdAt: updatePatient[0].createdAt ?? '',
+    updatedAt: updatePatient[0].updatedAt ?? '',
   }
   return patientData;
 }
