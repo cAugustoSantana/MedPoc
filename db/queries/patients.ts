9b0c4e5de3f3692ddd37b0bd126b863f6e19276c
nrg <itsenergyy05@gmail.com>
2025-06-21 12:39:31 -0400

    create - query

--- db/queries/patients.ts
+++ db/queries/patients.ts
@@ -18,3 +18,28 @@ export async function getAllPatients(): Promise<Patient[]> {
   }));
   return patientsData;
 }
+
+export async function createPatient(newPatient: Patient): Promise<Patient> {
+  const parsedPatient = {
+    name: newPatient.name,
+    email: newPatient.email,
+    dob: newPatient.dob,
+    gender: newPatient.gender,
+    phone: newPatient.phone,
+    address: newPatient.address,
+  };
+  const createdPatient = await db.insert(patient).values(parsedPatient).returning();
+  console.log("Created patient:", createdPatient);
+  const patientData: Patient = {
+    id: createdPatient[0].patientId.toString(),
+    name: createdPatient[0].name,
+    email: createdPatient[0].email ?? '',
+    dob: createdPatient[0].dob ?? '',
+    gender: createdPatient[0].gender ?? '',
+    phone: createdPatient[0].phone ?? '',
+    address: createdPatient[0].address ?? '',
+    createdAt: createdPatient[0].createdAt ?? '',
+    updatedAt: createdPatient[0].updatedAt ?? '',
+  }
+  return patientData;
+}
